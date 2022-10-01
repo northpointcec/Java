b6c1df383262994d6fe7a104daf2b9b213468222
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-01 23:22:27 +0530

    add

--- src/bitManipulation/Swap.java
+++ src/bitManipulation/Swap.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Swap {
 
-    public static void mian(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         sc.close();
-        System.out.println("Before swap a = "+a+"and b ="+b);
+        System.out.println("Before swap a = "+a+" and b ="+b);
         a = a^b;
         b = a^b;
         a = a^b;
-        System.out.println("After swap a = "+a+"and b = "+b);
+        System.out.println("After swap a = "+a+" and b = "+b);
     }
 }
