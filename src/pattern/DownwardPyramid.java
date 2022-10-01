22ee03c00a74aa4a76c06ef1f9ae58e207101d5c
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-01 17:39:35 +0530

    add

--- src/pattern/DownwardPyramid.java
+++ src/pattern/DownwardPyramid.java
@@ -10,7 +10,13 @@ public class DownwardPyramid {
 		int n = sc.nextInt();
 		sc.close();
 		for( int i = 1; i<=n; i++) {
-			
+			for( int j = 1; j<=i; j++) {
+			    System.out.print(" ");
+			}
+			for( int j = i; j<=n; j++) {
+			    System.out.print("* ");
+			}
+			System.out.println();
 		}
 	}
 }
