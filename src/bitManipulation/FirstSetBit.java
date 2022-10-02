da3685464788e8589ae2e739e27e2cf08b2132f6
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-02 14:04:28 +0530

    add

--- src/bitManipulation/FirstSetBit.java
+++ src/bitManipulation/FirstSetBit.java
@@ -6,17 +6,8 @@ public class FirstSetBit {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        //int n = sc.nextInt();
         sc.close();
-        int pos = 1;
-        int count = 0;
-        while(n>0) {
-            count++;
-         if((n&pos)==1) {
-            
-             break;
-         }
-        }
-        System.out.println(count);
+      
     }
 }
