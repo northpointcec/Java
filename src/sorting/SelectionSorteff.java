b857ac81b708d1bdeeae68f54e260bf7e0370b02
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-07 20:30:54 +0530

    add

--- src/sorting/SelectionSorteff.java
+++ src/sorting/SelectionSorteff.java
@@ -4,7 +4,7 @@ public class SelectionSorteff {
 
     public static void main(String[] args) {
         int[] arr = {9,8,7,6,5,4,3,2,1};
-        for( int i = 0; i<arr.length; i++){
+        for( int i = 0; i<arr.length-1; i++){
           int min = i;
             for( int j = i+1; j<arr.length; j++) {
                 if(arr[min]>arr[j]) {
