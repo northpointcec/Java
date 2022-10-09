9b056955b4162074479b874d17d3a1d128b266dd
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-09 09:39:47 +0530

    add

---
+++ src/collectionFrame/ComparatorDemo3.java
@@ -0,0 +1,31 @@
+package collectionFrame;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class ComparatorDemo3 {
+
+    public static void main(String[] args) {
+        TreeSet<Integer> t = new TreeSet<>(new myComparator1());
+            t.add(10);
+            t.add(0);
+            t.add(15);
+            t.add(20);
+            t.add(5);
+            t.add(20);
+        System.out.println(t);
+    }
+}
+class myComparator1 implements Comparator<Object>{
+    public int compare(Object obj1 , Object obj2) {
+        Integer a = (Integer)obj1;
+        Integer b = (Integer)obj2;
+        if(a>b) {
+            return +1;
+        }else if(a<b) {
+            return -1;
+        }else {
+            return 0;
+        }
+    }
+}
