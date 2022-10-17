f5022c772ffbff2de9bc81b5f18f6fa9cbcfa405
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-17 14:35:55 +0530

    add

--- src/mathematics/IsPrime.java
+++ src/mathematics/IsPrime.java
@@ -20,7 +20,7 @@ public class IsPrime {
         if( n%2==0 || n%3==0) {
             return false;
         }
-        for( int i = 5; i*i<n; i= i+6) {
+        for( int i = 5; i*i<=n; i= i+6) {
             if(n%i==0 || n%(i+2)==0) {
                 return false;
             }
