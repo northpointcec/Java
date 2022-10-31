58448ae70bb2220f1bf28ee5864539aea50b0896
ajay chaudhary <ajayvarma65845@gmail.com>
2022-10-31 21:00:29 +0530

    add

---
+++ src/langpackage/Immutablity.java
@@ -0,0 +1,10 @@
+package langpackage;
+
+public class Immutablity {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Ajay");
+        sb.append(" chaudhary");
+        System.out.println(sb);
+    }
+}
---
+++ src/langpackage/Mutablity.java
@@ -0,0 +1,10 @@
+package langpackage;
+
+public class Mutablity {
+
+    public static void main(String[] args) {
+        String s = new String("Ajay");
+        s.concat("chaudhary");
+        System.out.println(s);
+    }
+}
