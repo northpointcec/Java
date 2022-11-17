3f49dbeba62ac5873b72325227858eb3290487b1
ajay chaudhary <ajayvarma65845@gmail.com>
2022-11-17 19:24:18 +0530

    add

--- src/bitManipulation/Power.java
+++ src/bitManipulation/Power.java
@@ -2,13 +2,13 @@ package bitManipulation;
 
 import java.util.Scanner;
  
-public class Countsetbit {
+public class Power {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        int a = (int) (Math.log(n)/Math.log(2) + 1);
+        int a = (int) (Math.log(n)/Math.log(2) );
         System.out.println(a);
     }
 }
