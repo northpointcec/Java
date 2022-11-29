829d5e0fe34db40a2df92a004c06e69c840bc81a
ajay chaudhary <ajayvarma65845@gmail.com>
2022-11-29 19:12:35 +0530

    add

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,20 @@
+package sorting;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5,2,4,6,1,3};
+        for( int i = 1; i<arr.length; i++) {
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        for(int a:arr) {
+            System.out.print(a + " ");
+        }
+    }
+}
