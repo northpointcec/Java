23f00efe88ca1696994760f047b00f289481b336
ajay chaudhary <ajayvarma65845@gmail.com>
2022-12-06 19:58:30 +0530

    add

---
+++ src/mathematics/IterativeCount.java
@@ -0,0 +1,21 @@
+package mathematics;
+
+import java.util.Scanner;
+
+public class IterativeCount {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int res = 1;
+        while(m>0) {
+            if((m&1) == 1) {
+                res = res*n;
+            }
+            m = m>>1;
+            n = n*n;
+        }
+        System.out.println(res);
+    }
+}
