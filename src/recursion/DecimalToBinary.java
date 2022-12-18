9ced5dcf19cc53a2cae89cac215e2656a5703dd8
ajay chaudhary <ajayvarma65845@gmail.com>
2022-12-18 21:46:56 +0530

    add

--- src/recursion/DecimalToBinary.java
+++ src/recursion/DecimalToBinary.java
@@ -2,7 +2,7 @@ package recursion;
 
 import java.util.Scanner;
 
-public class BinaryToDecimal {
+public class DecimalToBinary {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
